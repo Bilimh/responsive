24181965b06c0025a0a824d7e59bc87635805cde
bil <bilal23@Host-003.lan>
2023-11-20 13:46:42 +0100

    new modif

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 import './style.css'
 
-const filtre = document.querySelector('.filtre');
+/*const filtre = document.querySelector('.filtre');
 const iconSearch = document.querySelector('.icon_search');
 const iconOpen = document.querySelector('.icon_open');
 const fermer =  document.querySelector('.fermer');
-
+const body = document.body
 //const prixMin = document.querySelector('#prix_min')
 
 filtre.classList.add('filtre_masque')
@@ -14,6 +14,7 @@ fermer.addEventListener('click', ()=>{
     filtre.classList.remove('filtre_vu')
     //iconOpen.style.display ="flex"
     iconSearch.style.display ="none"
+    body.classList.remove('no-scroll')
 })
 
 iconOpen.addEventListener('click', ()=>{
@@ -21,4 +22,45 @@ iconOpen.addEventListener('click', ()=>{
     filtre.classList.add('filtre_vu')
     iconSearch.style.display ="flex"
     //iconOpen.style.display ="none"
-})
+    body.classList.add('no-scroll')
+})*/
+
+const filtre = document.querySelector('.filtre');
+const iconSearch = document.querySelector('.icon_search');
+const iconOpen = document.querySelector('.icon_open');
+const fermer = document.querySelector('.fermer');
+const body = document.body;
+
+filtre.classList.add('filtre_masque');
+
+fermer.addEventListener('click', () => {
+    fermerFiltre();
+});
+
+iconOpen.addEventListener('click', () => {
+    ouvrirFiltre();
+});
+
+// Ajouter un gestionnaire d'événements pour le formulaire
+/*document.querySelector('#formulaire').addEventListener('submit', (e) => {
+    // Empêcher le rechargement de la page
+    e.preventDefault();
+    // Continuer le traitement du formulaire si nécessaire
+    // ...
+    // Fermer la barre de navigation
+    fermerFiltre();
+});*/
+
+function ouvrirFiltre() {
+    filtre.classList.remove('filtre_masque');
+    filtre.classList.add('filtre_vu');
+    iconSearch.style.display = "flex";
+    body.classList.add('no-scroll');
+}
+
+function fermerFiltre() {
+    filtre.classList.add('filtre_masque');
+    filtre.classList.remove('filtre_vu');
+    iconSearch.style.display = "none";
+    body.classList.remove('no-scroll');
+}
